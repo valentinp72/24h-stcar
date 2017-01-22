23878a077165fc390b8e039fc07c7e034589e809
Valentin Pelloin <valentinpelloin@gmail.com>
2017-01-22 10:10:01 +0100

    /w t2

--- control/main.c
+++ control/main.c
@@ -3,7 +3,124 @@
 #include "commands.h"
 #include "display.h"
 
+enum {VN1 = -20, V0 = 0, V1 = 20, V2 = 45, V3 = 60};
 
+typedef struct {
+	int speed;
+	double lat;
+	double lon;
+	double distToNext;
+} t_point;
+
+typedef struct {
+	double lat;
+	double lon;
+	int indexLastPoint;
+	double distToNext;
+} t_pos;
+
+typedef struct {
+	double lat;
+	double lon;
+} t_vect;
+
+double dist2Coor (t_point a, t_point b) {
+	double col = (b.lon - a.lon) * cos((a.lat + b.lat) / 2);
+	double line = b.lat - a.lat;
+	double dist = sqrt(col*col + line*line) * 6371.0; /*earth radius 6371 km*/
+	return dist;
+}
+
+int askSpeedToCar (int speed) {
+	return speed;
+}
+
+double getSpeed (int desireSpeed) {
+	int actualSpeed = askSpeedToCar(desireSpeed);
+	switch (actualSpeed) {
+		case -1: return VN1;
+		case  0: return V0;
+		case  1: return V1;
+		case  2: return V2;
+		case  3: return V3;
+		default: return V0;
+	}
+}
+
+double distPosCoor (t_pos pos, t_point point) {
+	double col = (point.lon - pos.lon) * cos((pos.lat + point.lat) / 2);
+	double line = point.lat - pos.lat;
+	double dist = sqrt(col*col + line*line) * 6371.0; /*earth radius 6371 km*/
+	return dist;
+}
+
+t_point * getCircuit (int nbPoint);
+int translateXML (int *nbLine);
+
+void tellServer (double lat, double lon) {
+	static int acc = 0;
+	char msg[400];
+	sprintf (msg, "wget --spider -q   --no-cookies --no-http-keep-alive  http://traccar.spc5studio.com:5055/\\?id\\=000001\\&lat\\=%.16lf\\&lon\\=%.16lf\\&altitude\\=72", lat, lon);
+	printf("%d", acc++);
+	fflush(stdout);
+	system (msg);
+	printf(" [V]\n");
+}
+
+
+t_point * getCircuit (int nbPoint) {
+	t_point *circuit = malloc(nbPoint * sizeof(t_point));
+	FILE * circuitFile = fopen ("circuit.txt", "r");
+
+	int speed, i;
+	double lat, lon;
+	for (i = 0; i < nbPoint; i++) {
+		fscanf (circuitFile, "%d %lf %lf", &speed, &lat, &lon);
+		circuit[i] = (t_point) {speed, lat, lon};
+	}
+
+	fclose(circuitFile);
+	return circuit;
+}
+
+int translateXML (int *nbLine) {
+	FILE *xml = NULL, *dst = NULL;
+	xml = fopen("CircuitBugatti.xml", "r");
+	if (xml == NULL) return -1;
+	dst = fopen("circuit.txt", "w");
+	int acc = 0;
+	int nbPoint = 0;
+	char c;
+	double number;
+	int integer;
+	int fileIsNotFinish;
+	do {
+		while (fscanf(xml, "%c", &c), c != '\"' && (fileIsNotFinish = !feof(xml)));
+
+		if (fileIsNotFinish) {
+			if (acc == 0) {
+				fscanf (xml, "%d", &integer);
+				fprintf(dst, "%d", integer);
+			}
+			else {
+				fscanf (xml, "%lf", &number);
+				fprintf(dst, "%.16lf", number);
+			}
+			fscanf(xml, "%*c");
+			acc++;
+			if (acc == 3) {
+				fprintf(dst, "\n");
+				nbPoint++;
+				acc = 0;
+			}
+			else fprintf(dst, " ");
+		}
+	} while (fileIsNotFinish);
+	*nbLine = nbPoint;
+	fclose(xml);
+	fclose(dst);
+	return 0;
+}
 
 int main(){
 
@@ -12,6 +129,30 @@ int main(){
     int quit = FALSE;
 	int i, found = FALSE, data = 0;
 	int isLampOn = FALSE, isTaillampOn = FALSE, isWindscreenOn = FALSE;
+
+
+
+	int nbPoint;
+	translateXML (&nbPoint);
+	t_point *circuit = getCircuit(nbPoint);
+	t_pos pos, lastPos;
+
+
+	for (i = 0; i < nbPoint - 1; i++) {
+		circuit[i].distToNext = dist2Coor(circuit[i], circuit[i+1]);
+	}
+	circuit[i].distToNext = dist2Coor (circuit[i], circuit[0]);
+
+	pos.lat = circuit[0].lat;
+	pos.lon = circuit[0].lon;
+	pos.indexLastPoint = 0;
+	pos.distToNext = circuit[0].distToNext;
+	t_vect vect = {-1, -1}, lastvect;
+	double angle;
+
+
+
+
 
 	initSdl(HEIGHT, WIDTH, "Télécommande");
 
@@ -157,9 +298,18 @@ int main(){
 		   	 }
 	 	}
 
-		/*currentTime = SDL_GetTicks();
+
+
+
+
+
+
+
+
+
+		currentTime = SDL_GetTicks();
 		if(currentTime > lastTime + 30) {
-        	//printf("ffsdf\n");
+        	/*//printf("ffsdf\n");
 			initRandom();
 			if(didItHappen(50)){
 				data = readData();
@@ -168,12 +318,56 @@ int main(){
 				displayText(counter, roboto, counterColor, 500, 20, 1);
 			}
 			render();
+			*/
+
+			lastvect = vect;
+			lastPos = pos;
+			double speed = getSpeed(circuit[pos.indexLastPoint].speed);
+			printf("%lf\n", speed);
+			double distToTravel = speed * 10 / 3600.0;
+			//printf("%.16lf %.16lf\n", distToTravel, pos.distToNext);
+			while (distToTravel  >= pos.distToNext) {
+				//printf("%.16lf %.16lf %.16lf\n", distToTravel, pos.distToNext, circuit[0].distToNext);
+				distToTravel -= pos.distToNext;
+				pos.indexLastPoint++;
+				if (pos.indexLastPoint == nbPoint) pos.indexLastPoint = 0;
+				int index = pos.indexLastPoint;
+				pos.lat = circuit[index].lat;
+				pos.lon = circuit[index].lon;
+				pos.distToNext = circuit[index].distToNext;
+			}
+
+			double varLon = circuit[(pos.indexLastPoint + 1)%nbPoint].lon - circuit[pos.indexLastPoint].lon;
+			double varLat = circuit[(pos.indexLastPoint + 1)%nbPoint].lat - circuit[pos.indexLastPoint].lat;
+
+			double facteur = distToTravel / circuit[pos.indexLastPoint].distToNext;
+			pos.lon += (varLon * facteur);
+			pos.lat += (varLat * facteur);
+
+			pos.distToNext -= distToTravel;
+			vect = (t_vect) {pos.lat - lastPos.lat, pos.lon - lastPos.lon};
+			if (lastvect.lat != -1 && lastvect.lon != -1) {
+				double terma = (lastvect.lat * vect.lon + lastvect.lon * vect.lat);
+				double termb = sqrt(lastvect.lat * lastvect.lat + lastvect.lon * lastvect.lon);
+				double termc = sqrt(vect.lat * vect.lat + vect.lon * vect.lon);
+				angle = acos (terma/(termb*termc));
+				if (angle *180 /3 > 10){
+					//execCommand("turn 1");
+				}
+				else if(angle * 180 /3 < -10){
+					//execCommand("turn 2");
+				}
+
+			}
+
+			tellServer (pos.lat, pos.lon);
+
 
         	lastTime = currentTime;
 
     	} else {
 			SDL_Delay(30 - (currentTime - lastTime));
-		}*/
+		}
 	}
 
 
